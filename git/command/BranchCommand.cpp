9e58d55e7e58eb14d2f2a247f64f491d695c42a4
DT-Dtechnology <DT-Dtechnology@outlook.com>
2018-07-27 16:42:14 +0800

    Dong Haowen

--- git/command/BranchCommand.cpp
+++ git/command/BranchCommand.cpp
@@ -6,31 +6,23 @@ void branch_command()
 {
 	const string current = FL_OP::get_current_branch();
 	cout << "Current Branch is: <"<<current <<">" << endl;
-	// TODO:Arthur
-	// TODO:�������ݿ�ʵ�ֻ�ȡ���з�֧���Ƶ�ָ��
-
 }
 
 void branch_command(const string& to_branch)
 {
-	// ��ȡ��ǰ��֧��
 	const string from_branch = FL_OP::get_current_branch();
-
 	branch_command(from_branch, to_branch);
 }
 
 void branch_command(const string& from_branch, const string& to_branch)
 {
-	// TODO:Arthur
-	// TODO:����֧������
+	if(DB_OP::get_Branch_Hash(from_branch) == NONE_FILE_HASH)
+		throw Error("No such branch.\n");
 
-	// ����ԭ��֧�����·�֧(ע����ķ�֧���ƺ�his_id)
 	Branch new_branch(from_branch);
 	new_branch.set_start(to_branch);
 
-	// ����Branch_Match��Ϣ
 	DB_OP::Branch_Match_Insert(new_branch);
 
-	// TODO:Scott
-	// TODO:���Branch��GIT_REF ����ʱ��֪����ʲô�ã���
+	new_branch.write();
 }
--- git/command/Command.h
+++ git/command/Command.h
@@ -5,11 +5,9 @@
  * We create a list of all the Command we may need.
  * Just include this file will help use all the Commands.
  */
-#include "AddCommand.h"
 #include "InitCommand.h"
 #include "BranchCommand.h"
 #include "CheckoutCommand.h"
-#include "CommitCommand.h"
 #include "DiffCommand.h"
 #include "DropCommand.h"
 #include "FetchCommand.h"
